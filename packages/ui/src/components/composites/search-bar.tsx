e9f67362cf3c9a6fe066d166b87cb200606da7d2
Jacob Juul Larsen <jjl@signifly.com>
2024-12-22 21:22:41 +0100

    feat(package-ui): enhance SearchBar component with form status handling and improved button states

--- packages/ui/src/components/composites/search-bar.tsx
+++ packages/ui/src/components/composites/search-bar.tsx
@@ -2,14 +2,26 @@
 
 import { Button } from '../ui/button.js'
 import { Input } from '../ui/input.js'
+import { useFormStatus } from 'react-dom'
 
 type Props = {
-  inputPlaceholder: string
-  buttonText: string
   buttonAction?: (query: string) => void
 }
 
-export const SearchBar = ({ inputPlaceholder, buttonText, buttonAction }: Props) => {
+const FormFields = () => {
+  const { pending } = useFormStatus()
+
+  return (
+    <>
+      <Input placeholder={'Search...'} name="query" disabled={pending} />
+      <Button type="submit" disabled={pending}>
+        {pending ? 'Pending...' : 'Search'}
+      </Button>
+    </>
+  )
+}
+
+export const SearchBar = ({ buttonAction }: Props) => {
   function search(formData: FormData): void {
     const query = formData.get('query') as string
 
@@ -20,8 +32,7 @@ export const SearchBar = ({ inputPlaceholder, buttonText, buttonAction }: Props)
 
   return (
     <form action={search} className="flex space-x-2">
-      <Input placeholder={inputPlaceholder} name="query" />
-      <Button type="submit">{buttonText}</Button>
+      <FormFields />
     </form>
   )
 }
